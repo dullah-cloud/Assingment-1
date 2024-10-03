f76fdc082f23811c8da66bd33ba1b1a98fa23079
dullah-cloud <abdallatsuma13@gmail.com>
2024-10-03 14:27:34 +0300

    Update Number 1.c

--- Number 1.c
+++ Number 1.c
@@ -1,24 +1,15 @@
-/*program to print loan qualification criteria
-Author : Tsuma Abdhalla
-Reg no ED102/G/19356/23
-Course : Technical Education
-Date : 3/10/2024
-*/
 #include <stdio.h>
 
 int main() {
     int age;
     float income;
     
-    // Prompt user for their age
     printf("Enter your age: ");
     scanf("%d", &age);
 
-    // Prompt user for their annual income
     printf("Enter your annual income in Sh: ");
     scanf("%f", &income);
 
-    // Prompt user for their annual income
     if (age >= 21 && income >= 21000) {
         printf("Congratulations, you qualify for a loan.\n");
     } else {
